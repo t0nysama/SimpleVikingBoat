51f641f3b6fb78541d6e93e22405519182746dea
t0nysama <moistresidue@gmail.com>
2022-09-06 13:49:11 -0400

    Initial Commit

---
+++ src/main/java/net/t0nysama/simplevikingboat/ClientInit.java
@@ -0,0 +1,11 @@
+package net.t0nysama.simplevikingboat;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientInit implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+
+	}
+}
---
+++ src/main/java/net/t0nysama/simplevikingboat/TonysVikingBoatMod.java
@@ -0,0 +1,16 @@
+package net.t0nysama.simplevikingboat;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TonysVikingBoatMod implements ModInitializer {
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Initializing TonysVikingBoatMod");
+
+	}
+}
