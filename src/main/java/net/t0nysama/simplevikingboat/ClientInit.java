d28065f1b21f744f2a54c8535a012b7f8a0e8474
t0nysama <moistresidue@gmail.com>
2022-09-09 04:05:20 -0400

    added model
    - not sure how to fix render code

--- src/main/java/net/t0nysama/simplevikingboat/ClientInit.java
+++ src/main/java/net/t0nysama/simplevikingboat/ClientInit.java
@@ -1,11 +1,16 @@
 package net.t0nysama.simplevikingboat;
 
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.EnvType;
+
+
 import net.fabricmc.api.ClientModInitializer;
 
+@Environment(EnvType.CLIENT)
 public class ClientInit implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 
-
 	}
 }
+
--- src/main/java/net/t0nysama/simplevikingboat/TonysVikingBoatMod.java
+++ src/main/java/net/t0nysama/simplevikingboat/TonysVikingBoatMod.java
@@ -1,5 +1,4 @@
 package net.t0nysama.simplevikingboat;
-
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
